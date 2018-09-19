9a3390b86f1cd58bc748ae9a6cb03312246bfec4
Dave <dave.samson91@gmail.com>
2018-09-19 22:38:44 +0000

    Added partials for user, polls, votes table

---
+++ db/migrations/20180919211623_create_polls_table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable("polls", function (table) {
+        table.increments("poll_id");
+        table.string("vote_url",255);
+        table.date("date_created");
+        table.date("date_expired");
+        table.string("poll_name",255);
+        table.jsonb("poll_options");
+        table.integer("user_id").references("user_id").inTable("users");
+    });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable("polls");
+};
--- db/seeds/users.js
+++ db/seeds/users.js
@@ -2,9 +2,9 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice'}),
-        knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
+        knex('users').insert({ email: "thofeeq@wolfpack.com"}),
+        knex('users').insert({ email: "aaron@wolfpack.com"}),
+        knex('users').insert({ email: "dave@wolfpack.com"})
       ]);
     });
 };
