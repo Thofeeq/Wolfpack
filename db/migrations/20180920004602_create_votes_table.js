0a3a12a70174b41d22624c7273a81cc1de936152
Dave <dave.samson91@gmail.com>
2018-09-20 22:07:15 +0000

    updated db, dropped users table added email to polls table, created unique poll id

--- db/migrations/20180920004602_create_votes_table.js
+++ db/migrations/20180920004602_create_votes_table.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable("votes", function (table) {
         table.increments("vote_id");
-        table.integer("poll_id").references("poll_id").inTable("polls");
+        table.string("poll_id").references("poll_id").inTable("polls");
         table.string("voter_name",50);
         table.jsonb("results");
     });
