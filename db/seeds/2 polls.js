6ee1b14247a0d0ce0ad358af660d7c7985254f04
Dave <dave.samson91@gmail.com>
2018-09-20 02:36:15 +0000

    Seeded Votes

--- db/seeds/2 polls.js
+++ db/seeds/2 polls.js
@@ -7,7 +7,19 @@
             date_expired:"2018-08-25",
             poll_name:"what's for dinner?",
             poll_options:{1:'tacos', 2:'pasta'},
-            user_id: 3})
+            user_id: 3}),
+            knex('polls').insert({vote_url:"GiraffeAardvark",
+            date_created:"2018-09-16",
+            date_expired:"2018-09-23",
+            poll_name:"What Movie Should We Watch?",
+            poll_options:{1:'The Grand Budapest Hotel', 2:'Skyfall', 3:'Isle of dogs'},
+            user_id: 2}),
+            knex('polls').insert({vote_url:"ParrotGuineaPig",
+            date_created:"2018-07-16",
+            date_expired:"2018-08-16",
+            poll_name:"Books for Bookclub?",
+            poll_options:{1:'Red Rising', 2:'Riyria Revelations', 3:'Ready Player One', 4:"His Dark Materials"},
+            user_id: 1})
         ]);
     });
 }; 
