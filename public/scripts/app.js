45eec90d044de758605e8cde4091fc32d9da4975
Aaron Parsons <aaron.parsons@live.ca>
2018-09-21 00:51:47 +0000

    fixed merge conflict

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,18 @@
 
 $(document).ready(function () {
-  
+
   $("#poll-page-container").hide();
   $(".errors").hide();
   // $("#date").flatpickr({enableTime:true});
   $(".features-box").hide();
   $(".features-box").slideToggle(1000).show();
-<<<<<<< HEAD
 
   $("#btn-register").click(function(){
     $("#login-page-container").hide(("slide", {direction: "right"}, 1000));
-    $("#poll-page-container").show();   
+    $("#poll-page-container").show();
    return false;
   });
-  
+
 
   $(".date-picker").flatpickr({enableTime:true});
 
@@ -26,35 +25,35 @@ $(document).ready(function () {
       {
         var hasWhiteSpace =  true;
       }
-     
+
       if(s === ""){
         var emptyString = true;
       }
-      
+
       if(hasWhiteSpace || emptyString)
       {
-        emptyOuput = true; 
+        emptyOuput = true;
       }
 
       return emptyOuput;
     }
-    
+
     var requireChoice1 = $("#req-choice-1").val();
     var requireChoice2 = $("#req-choice-2").val();
 
     if(isStringEmpty(requireChoice1) || isStringEmpty(requireChoice2))
     {
       $("#fieldEmptyError").show();
-     
-      
+
+
     }
     else{
       $("#fieldEmptyError").hide();
       $("#option-container").append('<input type="text" class="choices"/>');
     }
-   
+
   })
- 
+
   // ajax call to add user to database
   // $('#login-form').on('submit', function(e) {
   //   e.preventDefault();
@@ -79,7 +78,6 @@ $(document).ready(function () {
   // $('#vote-form').on('submit', function(e) {
 
   // });
-=======
   $("#date").flatpickr({enableTime:true});
 
   $('#submit-poll').on('submit', function(e) {
@@ -94,7 +92,6 @@ $(document).ready(function () {
       console.log('Login-form ajax call completed');
     });
   });
->>>>>>> 6f8e395c02b9daa09207acc8d3240356891329af
 
 // });
 
