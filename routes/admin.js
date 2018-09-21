70464679332234bdca0dc14c0073c24875fc6cc2
Dave <dave.samson91@gmail.com>
2018-09-21 02:01:23 +0000

    Added get route with access to both tables in db

--- routes/admin.js
+++ routes/admin.js
@@ -5,10 +5,17 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
-  // Get admin dashboard page
+  // Get admin results page
   router.get('/:id', (req, res) => {
-
+    const pollId = req.params.id;
+    knex.select("*")
+    .from("votes")
+    .join("polls",{"polls.poll_id" : "votes.poll_id"})
+    .where({'votes.poll_id' : pollId})
+    .then((results) => {
+      res.render("admin-results", results[0]);
+    });
   });
 
   return router;
-}
+};
