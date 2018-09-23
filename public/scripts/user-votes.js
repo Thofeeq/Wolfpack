95825c4aef1eb770bd849660e913b4e89fa63a47
Aaron Parsons <aaron.parsons@live.ca>
2018-09-23 21:05:41 +0000

    added name input on voting page

--- public/scripts/user-votes.js
+++ public/scripts/user-votes.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
+  $('.name-submit-btn').on('click', function() {
+    if ($('#voter-name').val() === '') {
+      $('#voter-name-err').show();
+      // $('#voter-name').css("border", "1px red solid");
+    } else {
+      $('#voter-name-input').slideUp(function() {
+        $('#user-vote-poll-container').slideDown().css('display', 'flex');
+      });
+    }
+  });
+
   $( "#sortable" ).sortable();
   $( "#sortable" ).disableSelection();
 
